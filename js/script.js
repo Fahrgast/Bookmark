02d57c3f80f71f2e3419d83af002aeb07c764ab3
Fahrgast <felix_argast@gmx.de>
2022-08-12 15:51:02 +0200

    Email validation added

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
 /* ----- Mobile Nav Overlay ----- */
-nav_toggle = document.querySelector(".navbar-toggler");
-nav_overlay = document.querySelector(".mobile-nav-overlay");
-nav_logo = document.querySelector(".navbar-branding");
-hambuger = document.querySelector(".navbar-toggler");
+const nav_toggle = document.querySelector(".navbar-toggler");
+const nav_overlay = document.querySelector(".mobile-nav-overlay");
+const nav_logo = document.querySelector(".navbar-branding");
+const hamburger = document.querySelector(".navbar-toggler");
 
 nav_toggle.addEventListener("click", function() {
     nav_overlay.classList.toggle("mobile-nav-overlay-active");
     nav_logo.classList.toggle("mobile-nav-hidden");
-    hambuger.classList.toggle("mobile-nav-hidden");
+    hamburger.classList.toggle("mobile-nav-hidden");
 })
 
 nav_toggle_close = document.querySelector(".mobile-nav-overlay__closer")
 nav_toggle_close.addEventListener("click", function() {
     nav_overlay.classList.toggle("mobile-nav-overlay-active");
     nav_logo.classList.toggle("mobile-nav-hidden");
-    hambuger.classList.toggle("mobile-nav-hidden");
+    hamburger.classList.toggle("mobile-nav-hidden");
 })
 
 
@@ -57,7 +57,37 @@ for (let x = 0; x < toggler.length; x++) {
         }
       });
     }
-    
+
+const error_message = document.querySelector(".fieldset_email");
+const error_icon = document.querySelector(".newsletter__content__form__error");
+const input = document.querySelector(".newsletter__content__form__email");
+const submit = document.querySelector(".newsletter__content__form");
+const email_legend = document.querySelector(".email_legend");
+
+input.addEventListener('invalid', function (event) {
+  event.preventDefault();
+  if(!error_message.classList.contains("fieldset_active"))
+  {
+    error_message.classList.toggle("fieldset_active");
+    error_icon.style.display = "block";
+    email_legend.style.display = "block";
+  } 
+})
+
+
+
+submit.addEventListener('submit', function(event){
+  event.preventDefault();
+  if(error_message.classList.contains("fieldset_active"))
+  {
+    error_message.classList.toggle("fieldset_active");
+    error_icon.style.display = "block";
+    email_legend.style.display = "none";
+  } 
+  alert("Thanks for contacting us!");
+})
+
+
 
 /*                   
 .active{
