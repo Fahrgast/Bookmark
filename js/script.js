43c847e85746d64928e96c69cc82ed89501960c9
Fahrgast <felix_argast@gmx.de>
2022-08-12 13:55:12 +0200

    mobile - done

--- js/script.js
+++ js/script.js
@@ -1,29 +1,47 @@
+/* ----- Mobile Nav Overlay ----- */
+nav_toggle = document.querySelector(".navbar-toggler");
+nav_overlay = document.querySelector(".mobile-nav-overlay");
+nav_logo = document.querySelector(".navbar-branding");
+hambuger = document.querySelector(".navbar-toggler");
 
-nav_toggle = document.querySelector(".navbar-toggler-icon")
 nav_toggle.addEventListener("click", function() {
-    
-    document.body.classList.toggle("mobile_nav_active");
+    nav_overlay.classList.toggle("mobile-nav-overlay-active");
+    nav_logo.classList.toggle("mobile-nav-hidden");
+    hambuger.classList.toggle("mobile-nav-hidden");
 })
+
+nav_toggle_close = document.querySelector(".mobile-nav-overlay__closer")
+nav_toggle_close.addEventListener("click", function() {
+    nav_overlay.classList.toggle("mobile-nav-overlay-active");
+    nav_logo.classList.toggle("mobile-nav-hidden");
+    hambuger.classList.toggle("mobile-nav-hidden");
+})
+
+
 
 /* ----- Features List Toggler ----- */
 var acc = document.getElementsByClassName("features__list__button");
-var i;
 
-for (i = 0; i < acc.length; i++) {
+for (let i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
     /* Toggle between hiding and showing the active panel */
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
+    var panel = document.querySelectorAll(".feature_tab");
+    for(let x = 0; x < acc.length; x++){
+      if(x === i){
+        if (panel[i].style.display === "block") {
+          panel[i].style.display = "none";
+        } else {
+          panel[i].style.display = "block";
+        }
+      }else{
+        panel[x].style.display = "none";
+      }
     }
   });
 }
 
 /* ----- Faq Toggler ----- */
 var toggler = document.getElementsByClassName("faq__list__item__heading__button");
-
 
 for (let x = 0; x < toggler.length; x++) {
     toggler[x].addEventListener("click", function() {
